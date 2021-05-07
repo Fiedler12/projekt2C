0e9b60fd04f8ec46ee503da7ca54dd2108159ac5
UnoDosAndres <s205450@student.dtu.dk>
2021-05-07 23:15:23 +0200

    Poops

--- main.c
+++ main.c
@@ -4,12 +4,13 @@
 
 void fillCards();
 void mixCards();
-void layOut();
-int topCard;
+void layOut(int deckSize, int index);
 void printStack();
 void printBoard();
+void printLine(int lineIndex);
+struct Card *getListIndex(int lineIndex, int arrayIndex);
 
-
+int topCard;
 
 struct Card
 {
@@ -31,8 +32,19 @@ int main()
     fillCards();
     mixCards();
     topCard = 0;
-    layOut();
-    printStack();
+    layOut(7,1);
+    printBoard();
+    /*printf("C1");
+    struct Card *current = columns[1].ptr;
+    struct Card *next = current->next;
+    while (1) {
+        printf("%c%c", current->suit, current->value);
+        current = next;
+        next = current->next;
+        if (next == NULL) {
+            break;
+        }
+    } */
     return 0;
 }
 
@@ -66,34 +78,59 @@ void mixCards()
     }
 
 }
-
-void layOut() {
+//WORKS and poops and ze sprms
+void layOut(int deckSize, int index) {
     struct Card *current;
     current = &cards[topCard];
     struct Card *next;
-    c1.ptr = current;
-    ++topCard;
-    current->next = &cards[topCard];
-    next = current->next;
-    current = next;
+    columns[index].ptr = current;
 
-    for (int i = 0; i < 7; ++i) {
+    for (int i = 0; i < deckSize-1; ++i) {
         current->next = &cards[topCard + 1];
         next = current->next;
         current = next;
         topCard++;
     }
 }
+void printLine(int lineIndex) {
+    struct Card *current;
+    for (int i = 0; i < 7; ++i) {
+        current = getListIndex(lineIndex,i);
+        if (current == NULL){
+            printf("\t");
+        }
+        printf("%c%c\t", current->value, current->suit);
+    }
+}
+
+struct Card *getListIndex(int lineIndex, int arrayIndex) {
+    struct Card *result;
+    result = columns[arrayIndex].ptr;
+    struct Card *next;
+    next = result->next;
+    if (lineIndex == 1) {
+        return result;
+    }
+    for (int i = 0; i < lineIndex-1; ++i) {
+        result = next;
+        next = result->next;
+        if (next == NULL) {
+            break;
+        }
+    }
+    return result;
+}
+
 
 void printBoard() {
-    printf("C1\t C2 \t C3 \t C4 \t C5 \t C6 \t C7");
+    printf("C1\t C2 \t C3 \t C4 \t C5 \t C6 \t C7 \n\n");
+    printLine(1);
 }
-/*
+
 void printStack() {
-    printf("C1: %c%c ", c1.ptr->suit, c1.ptr->value);
     struct Card *current;
     struct Card *next;
-    current = c1.ptr;
+    current = columns[5].ptr;
     next = current->next;
     current = next;
         while (1) {
@@ -105,4 +142,3 @@ void printStack() {
             current = next;
         }
     }
-*/
