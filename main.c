8f6e48e316368282d917a6dbfcff74a2ce140ff4
Oliver <o.lyngholm@gmail.com>
2021-05-07 12:54:32 +0200

    FillCards

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 // Working of arithmetic operators
 #include <stdio.h>
+
+void fillCards();
+
+struct Card
+{
+    char suit;
+    char value;
+    struct Card* next;
+};
+
+struct Card cards[52];
+
 int main()
 {
-    
+
+
 }
+
+void fillCards()
+{
+    int t = 0;
+    char suits[4] = {'h', 'c', 'd', 's'};
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 13; ++j) {
+            cards[t].
+
+        }
+
+    }
+}
+
+
