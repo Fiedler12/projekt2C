7bd29984c3c73bddaf2bfe2cd9852ad310572020
UnoDosAndres <s205450@student.dtu.dk>
2021-05-07 13:19:32 +0200

    Seje values

--- main.c
+++ main.c
@@ -14,21 +14,27 @@ struct Card cards[52];
 
 int main()
 {
-
-
+    fillCards();
+    for (int i = 0; i < 52; ++i) {
+        printf("%c", cards[i].suit);
+        printf("%c \n", cards[i].value);
+    }
+    return 0;
 }
 
 void fillCards()
 {
     int t = 0;
-    char suits[4] = {'h', 'c', 'd', 's'};
+    char suits[5] = {'H', 'C', 'D', 'S','\0'};
+    char value[13] = {'1','2','3','4','5','6','7','8','9','T','J','Q','K'};
     for (int i = 0; i < 4; ++i) {
         for (int j = 0; j < 13; ++j) {
-            cards[t].
-
+            cards[t].suit = suits[i];
+            cards[t].value = value[i];
+            ++t;
         }
-
     }
+
 }
 
 
